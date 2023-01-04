90b90466231895b352f9d4d595c2966ef120903a
Ryan Spencer <ryan.spencer1220@gmail.com>
2023-01-03 22:08:28 -0800

    add newVideoLoaded function

--- background.js
+++ background.js
@@ -2,10 +2,14 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = URLSearchParams(queryParameters);
-
+    console.log(urlParameters);
     chrome.tabs.sendMessage(tabId, {
-      type: "new",
+      type: "NEW",
       videoId: urlParameters.get("v"),
     });
   }
+  const newVideoLoaded = () => {
+    const bookmarkBtnExists =
+      document.getElementsByClassName("bookmark-btn")[0];
+  };
 });
